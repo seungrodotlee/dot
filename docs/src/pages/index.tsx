b7669d6deb55cd3e0516c970a84cdca362449578
seungrodotlee <srlee0226w@gmail.com>
2024-02-09 00:15:04 +0900

    feat. Done with main logo

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,18 +1,112 @@
-import * as React from "react";
+import {
+  MouseEventHandler,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 
 import "twin.macro";
-import { StaticImage } from "gatsby-plugin-image";
+
+import { Link, type HeadFC, type PageProps } from "gatsby";
+import { useDynamicGeul } from "@dot/geul-react";
+import { delay } from "@fxts/core";
 
 import Layout from "../domains/@ui/layout/layout";
 import { StyledIndex } from "../styles/pages.styles";
 
-import { Link, type HeadFC, type PageProps } from "gatsby";
+const IndexPage = (props: PageProps) => {
+  const {
+    geul: wha,
+    isRunning: isWhaRunning,
+    run: runWha,
+  } = useDynamicGeul("", {
+    speed: 50,
+    decomposeOnBackspace: true,
+  });
 
-const IndexPage: React.FC<PageProps> = () => {
+  const {
+    geul: asy,
+    isRunning: isAsyRunning,
+    run: runAsy,
+  } = useDynamicGeul("", {
+    speed: 50,
+    decomposeOnBackspace: true,
+  });
+
+  const {
+    geul: eed,
+    isRunning: isEedRunning,
+    run: runEed,
+  } = useDynamicGeul("", {
+    speed: 50,
+    decomposeOnBackspace: true,
+  });
+
+  const hovered = useRef<boolean>(false);
+  const [transformed, setTransformed] = useState<boolean>(false);
+
+  const isRunning = useMemo(
+    () => isWhaRunning || isAsyRunning || isEedRunning,
+    [isWhaRunning, isAsyRunning, isEedRunning],
+  );
+
+  const mouseEnterHandler: MouseEventHandler<HTMLDivElement> =
+    useCallback(async () => {
+      hovered.current = true;
+      setTransformed(true);
+
+      await delay(2000);
+      if (!hovered.current) {
+        setTransformed(false);
+      }
+    }, [isRunning]);
+
+  const mouseLeaveHandler: MouseEventHandler<HTMLDivElement> =
+    useCallback(() => {
+      hovered.current = false;
+
+      if (!isRunning) {
+        setTransformed(false);
+      }
+    }, [isRunning]);
+
+  useEffect(() => {
+    if (hovered.current) {
+      runWha(" WHA");
+      runAsy("ASY");
+      runEed("EED ");
+    } else {
+      runWha("");
+      runAsy("");
+      runEed("");
+    }
+  }, [transformed]);
+
   return (
     <Layout withoutSidebar>
       <StyledIndex.Root>
-        <StyledIndex.Title>WELCOME TO DOT UNIVERSE</StyledIndex.Title>
+        <StyledIndex.Title
+          transformed={transformed}
+          onMouseEnter={mouseEnterHandler}
+          onMouseLeave={mouseLeaveHandler}
+        >
+          <span>DO</span>
+          <span>{wha}</span>
+          <span>T U</span>
+          <span className="trans-shrink">&nbsp;</span>
+          <span>N</span>
+          <span>{eed}</span>
+          <span>I</span>
+          <span className="trans-hidden">S&nbsp;</span>
+          <span>VER</span>
+          <span className="trans-show">S</span>
+          <span className="trans-hidden">Y</span>
+          <span className="trans-hidden">&nbsp;</span>
+          <span>E</span>
+          <span>{asy}</span>
+        </StyledIndex.Title>
         <StyledIndex.ContentWrap>
           <StyledIndex.Content>
             DOT은 JS/TS에서 풀스택에 걸쳐 사용할 수 있는
--- docs/src/styles/pages.styles.ts
+++ docs/src/styles/pages.styles.ts
@@ -9,12 +9,32 @@ export const StyledIndex = {
       ${tw`w-full`}
     }
   `,
-  Title: styled.div`${[tw`p-4`, utilStyles.font.black, utilStyles.font.size(20), utilStyles.decorate.underline]}`,
+  Title: styled.div<{ transformed: boolean }>`
+    ${[tw`p-4`, utilStyles.flex.center, utilStyles.font.black, utilStyles.font.size(12), utilStyles.decorate.underline]}
+    
+    & > * {
+      pointer-events: none;
+      white-space: pre;
+    }
+
+    & > .trans-shrink {
+      transition: font-size 0.4s cubic-bezier(0, 0.2, 1, 0.2);
+      font-size: ${({ transformed }) => transformed ? "1em" : 0};
+    }
+
+    & > .trans-hidden {
+      ${({ transformed }) => transformed ? "" : "display: none;"}
+    }
+
+    & > .trans-show {
+      ${({ transformed }) => transformed ? "display: none;" : ""}
+    }
+  `,
   ContentWrap: styled.div`
     ${[tw`grow`, utilStyles.flex.between]}
   `,
   Content: styled.p`
-    ${tw`grow w-full`}
+    ${tw`grow w-full p-4`}
   `,
   Links: styled.div`
     ${tw`flex flex-col items-end min-w-[320px] border-l border-black`}
